efea0c590af09c3a53310dd63d7ddb5e1e63fa38
Chris Glencross <chris@glencross.org>
2026-04-21 08:49:04 +0100

    Download from website, not Canva

--- update-video.sh
+++ update-video.sh
@@ -1,18 +1,25 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd $(dirname $0)
-source ./venv/bin/activate
 
-python -m captive_portal
-python -m canva_export
-status=$?
+# source ./venv/bin/activate
+# python -m captive_portal
 
-# Try to copy to persistent cache directory so we can display after a reboot
-if [[ $status -eq 0 ]]; then
-   cache=$HOME/data
-   if [[ -d $cache ]]; then
-     echo "Copying to persistent cache"
-     cp DAHB9p6lzXA.mp4 $cache/DAHB9p6lzXA.mp4 || true
-     cp DAHB9p6lzXA.mp4.ts $cache/DAHB9p6lzXA.mp4.ts || true
-   fi
+url="https://www.epsomandewellharriers.org/clubroom-video/"
+local_file=clubroom-video.mp4
+tmp_file="${local_file}.tmp"
+
+if [[ -f "$tmp_file" ]]; then
+  rm "$tmp_file"
 fi
+if [[ -f "$local_file" ]]; then
+  curl -s --fail -L -R -z "$local_file" -o "$tmp_file" "$url"
+else
+  curl -s --fail -L -R -o "$tmp_file" "$url"
+fi
+if [[ -f "$tmp_file" ]]; then
+  mv "$tmp_file" "$local_file"
+  echo "Updated $local_file"
+  ls -l "$local_file"
+fi
+
